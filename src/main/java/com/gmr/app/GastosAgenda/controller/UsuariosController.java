71824d382ff11624401c95be0d5babfac0cef05d
Gerardo Mandujano Roldan <gerardo.mandujano@imss.gob.mx>
2024-11-29 16:36:13 -0600

    cambiando el nombres

--- src/main/java/com/gmr/app/GastosAgenda/controller/UsuariosController.java
+++ src/main/java/com/gmr/app/GastosAgenda/controller/UsuariosController.java
@@ -29,7 +29,7 @@ public class UsuariosController {
 	
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ResponseEntity<ResponseDto> obtenerUsuariosaasasae(
+	public ResponseEntity<ResponseDto> obtenerUsuarios(
 			@RequestParam("idUsuario")String idUsuario 
 			){
 		ResponseDto response = new ResponseDto();
